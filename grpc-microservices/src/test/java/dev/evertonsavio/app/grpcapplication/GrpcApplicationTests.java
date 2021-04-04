eab38885459b4a2f378becc860a1dc39a6208371
havyx <everluca@outlook.com>
2021-04-04 16:05:30 -0300

    :tada: grpc microservices

---
+++ grpc-microservices/src/test/java/dev/evertonsavio/app/grpcapplication/GrpcApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.evertonsavio.app.grpcapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrpcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
