88f770e8f020476edaa48f5428103a645a1af6be
havyx <everluca@outlook.com>
2021-04-04 19:38:59 -0300

    :tada: grpc proto microservices

---
+++ grpc-microservices/users-service/src/main/java/dev/evertonsavio/grpc/usersservice/UsersServiceApplication.java
@@ -0,0 +1,13 @@
+package dev.evertonsavio.grpc.usersservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsersServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsersServiceApplication.class, args);
+	}
+
+}
