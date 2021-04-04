6bb9e7473f59f1046b14fe7971301c17e27fd190
havyx <everluca@outlook.com>
2021-04-04 13:09:40 -0300

    :sparkles: deadline unary & streaming timeout

---
+++ grpc-unary-streaming/src/test/java/dev/evertonsavio/app/client/deadline/DeadlineClientTest.java
@@ -0,0 +1,76 @@
+package dev.evertonsavio.app.client.deadline;
+
+import dev.evertonsavio.app.client.rpctypes.BalanceStreamObserver;
+import dev.evertonsavio.app.client.rpctypes.MoneyStramingResponse;
+import dev.evertonsavio.app.models.*;
+import io.grpc.Deadline;
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
+import io.grpc.StatusRuntimeException;
+import io.grpc.stub.StreamObserver;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS) //JUnit5 avoid needed for static method below
+public class DeadlineClientTest {
+
+    private BankServiceGrpc.BankServiceBlockingStub blockingStub;
+    private BankServiceGrpc.BankServiceStub bankServiceStub;
+
+    @BeforeAll
+    public void setup(){
+        ManagedChannel managedChannel = ManagedChannelBuilder.forAddress("localhost", 6565)
+                .usePlaintext()
+                .build();
+
+        this.blockingStub = BankServiceGrpc.newBlockingStub(managedChannel);
+        this.bankServiceStub = BankServiceGrpc.newStub(managedChannel);
+    }
+
+    @Test
+    public void balanceTest(){
+
+        BalanceCheckRequest balanceCheckRequest = BalanceCheckRequest.newBuilder()
+                .setAccountNumber(7)
+                .build();
+        try {
+            Balance balance = this.blockingStub
+                    .withDeadline(Deadline.after(2, TimeUnit.SECONDS))
+                    .getBalance(balanceCheckRequest);
+            System.out.println("Received: " + balance.getAmount());
+        }
+        catch (StatusRuntimeException e){
+            System.out.println(e);
+            //go with deafault value
+        }
+    }
+
+    @Test
+    public void withdrawTest() {
+        WithdrawRequest withdrawRequest = WithdrawRequest.newBuilder()
+                .setAccountNumber(7)
+                .setAmount(40).build();
+        try {
+            this.blockingStub
+                    .withDeadline(Deadline.after(4, TimeUnit.SECONDS))
+                    .withdraw(withdrawRequest)
+                    .forEachRemaining(money -> System.out.println("Received: " + money.getValue()));
+        } catch (StatusRuntimeException e) {
+            System.out.println(e);
+            //default...
+        }
+    }
+    @Test
+    public void withdrawAsyncTest() throws InterruptedException {
+        CountDownLatch latch = new CountDownLatch(1);
+        WithdrawRequest withdrawRequest = WithdrawRequest.newBuilder().setAccountNumber(10).setAmount(50).build();
+        this.bankServiceStub.withdraw(withdrawRequest, new MoneyStramingResponse(latch));
+        latch.await();
+        //Uninterruptibles.sleepUninterruptibly(3, TimeUnit.SECONDS);
+    }
+
+}
